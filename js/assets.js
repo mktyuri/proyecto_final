231b0f2c109f810e19f442da6afefb1850122d99
YURI MARTINEZ <martinezyuri@hotmail.com>
2021-09-20 21:16:11 -0300

    proyecto final

---
+++ js/assets.js
@@ -0,0 +1,11 @@
+swal({
+  title: "Bienvenido",
+  text: "Dirigete a Quiénes somos para asociarte!",
+  icon: "success",
+  button: "Ok",
+})
+
+$('.carousel').carousel({
+    interval: 2000
+  })
+
